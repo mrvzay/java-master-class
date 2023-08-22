5d859ece20589f14d8b28fd785b0dc25445be481
mrvzay <vzay1999@gmail.com>
2023-08-22 14:51:01 +0530

    add folders

--- Arrays/ArraysChallenge/src/Main.java
+++ Arrays/ArraysChallenge/src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -5,8 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[]  unsortedArray = getRandomArray(5);
+        int[] unsortedArray = getRandomArray(5);
         System.out.println(Arrays.toString(unsortedArray));
+
+        int[] sortedArray = sortIntegers(new int[] {7, 30, 35});
+        System.out.println(Arrays.toString(sortedArray));
 
     }
 
@@ -23,14 +27,22 @@ public class Main {
 
     private static int[] sortIntegers(int[] array) {
 
+        System.out.println(Arrays.toString(array));
         int[] sortedArray = Arrays.copyOf(array, array.length);
         boolean flag = true;
         int temp;
         while (flag) {
             flag = false;
-            for (int i = 0; i < sortedArray.length -1; i++) {
-                if (sortedArray[i])
+            for (int i = 0; i < sortedArray.length - 1; i++) {
+                if (sortedArray[i] < sortedArray[i + 1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i + 1] = temp;
+                    flag = true;
+                    System.out.println("--------->" + Arrays.toString(sortedArray));
+                }
             }
+            System.out.println("-----> " + Arrays.toString(sortedArray));
         }
 
         return sortedArray;
---
+++ Arrays/MinimumElementChallenge/src/dev/lpa/Main.java
@@ -0,0 +1,62 @@
+package dev.lpa;
+
+import javax.management.StandardEmitterMBean;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] returnedArray = readIntegers();
+        System.out.println(Arrays.toString(returnedArray));
+
+//        int returneMin = findMin(returnedArray);
+//        System.out.println("min = " + returneMin);
+
+        reverse(returnedArray);
+        System.out.println("Final: " + Arrays.toString(returnedArray));
+
+    }
+
+    private static int[] readIntegers() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a list a integers, separated by commas:");
+        String input = scanner.nextLine();
+
+        String[] splits = input.split(",");
+        int[] values = new int[splits.length];
+
+        for (int i = 0; i < splits.length; i++) {
+            values[i] = Integer.parseInt(splits[i].trim());
+        }
+
+        return values;
+    }
+
+    private static int findMin(int[] array) {
+
+        int min = Integer.MAX_VALUE;
+        for (int el : array) {
+            if (el < min) {
+                min = el;
+            }
+        }
+
+        return min;
+    }
+
+    private static void reverse(int[] array) {
+
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
+
+        for (int i = 0; i < halfLength; i++) {
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - 1] = temp;
+            System.out.println("----->" + Arrays.toString(array));
+        }
+    }
+}
