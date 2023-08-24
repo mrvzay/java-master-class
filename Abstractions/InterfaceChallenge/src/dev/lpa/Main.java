9b7f7b155a792b9c24e02ea0e346096b0b383aed
mrvzay <vzay1999@gmail.com>
2023-08-24 20:18:48 +0530

    add generics lectures

---
+++ Abstractions/InterfaceChallenge/src/dev/lpa/Main.java
@@ -0,0 +1,25 @@
+package dev.lpa;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Mappable> mappables = new ArrayList<>();
+
+        mappables.add(new Building("Sydney Town Hall", UsageType.GOVERNMENT));
+        mappables.add(new Building("Sydney Opera House", UsageType.ENTERTAINMENT));
+        mappables.add(new Building("Stadium Australia", UsageType.SPORTS));
+
+        mappables.add(new UtilityLine("College St", UtilityType.FIBER_OPTIC));
+        mappables.add(new UtilityLine("Olympic Blvd", UtilityType.WATER));
+
+        for (var m : mappables) {
+            Mappable.mapIt(m);
+        }
+    }
+}
