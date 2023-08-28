e8effe42d41fd6f55442d165f094636c4c63c3b1
mrvzay <vzay1999@gmail.com>
2023-08-28 12:45:21 +0530

    add more folders and lectures

--- NestedClasses/NestedClasses/src/dev/lpa/Main.java
+++ NestedClasses/NestedClasses/src/dev/lpa/Main.java
@@ -3,9 +3,12 @@ package dev.lpa;
 import com.sun.jdi.ArrayReference;
 import dev.lpa.domain.Employee;
 import dev.lpa.domain.EmployeeComparator;
+import dev.lpa.domain.StoreEmployee;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.zip.DeflaterOutputStream;
 
 public class Main {
 
@@ -26,6 +29,65 @@ public class Main {
 
         for (Employee e : employees) {
             System.out.println(e);
+        }
+
+        System.out.println("Store Members");
+        List<StoreEmployee> storeEmployees = new ArrayList<>(List.of(
+                new StoreEmployee(10015, "Meg", 2019, "Target"),
+                new StoreEmployee(10515, "Joe", 2021, "Walmart"),
+                new StoreEmployee(10105, "Tom", 2020, "Macys"),
+                new StoreEmployee(10215, "Marty", 2018, "Walmart"),
+                new StoreEmployee(10322, "Bud", 2016, "Target")
+        ));
+
+        var comparator = new StoreEmployee().new StoreComparator<>();
+        storeEmployees.sort(comparator);
+
+        for (StoreEmployee e : storeEmployees) {
+            System.out.println(e);
+        }
+
+        System.out.println("With Pig Latin Names");
+        addPigLatinName(storeEmployees);
+
+    }
+
+    public static void addPigLatinName(List<? extends StoreEmployee> list) {
+
+        String lastName = "Piggy";
+
+        class DecoratedEmployee extends StoreEmployee implements Comparable<DecoratedEmployee> {
+
+            private String pigLatinName;
+            private Employee originalInstance;
+
+            public DecoratedEmployee(String pigLatinName, Employee originalInstance) {
+                this.pigLatinName = pigLatinName + " " + lastName;
+                this.originalInstance = originalInstance;
+            }
+
+            @Override
+            public String toString() {
+                return originalInstance.toString() + " " + pigLatinName;
+            }
+
+            @Override
+            public int compareTo(DecoratedEmployee o) {
+                return pigLatinName.compareTo(o.pigLatinName);
+            }
+        }
+
+        List<DecoratedEmployee> newList = new ArrayList<>(list.size());
+
+        for (var employee : list) {
+            String name = employee.getName();
+            String pigLatin = name.substring(1) + name.charAt(0) + "ay";
+            newList.add(new DecoratedEmployee(pigLatin, employee));
+        }
+
+        newList.sort(null);
+        for (var dEmployee : newList) {
+            System.out.println(dEmployee.originalInstance.getName() + " " + dEmployee.pigLatinName);
         }
     }
 }
